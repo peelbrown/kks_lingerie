9d9037333ce120fd3891b5ca2680d9270310ee92
peelbrown <peelbrown@gmail.com>
2020-11-22 02:39:29 +0200

    Header in progress. Need to re-order menu, add animation.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,17 @@
 // ОТКЛЮЧИТЬ SCALE НА IOS. РАБОТАЕТ ЕСЛИ ТОЛЬКО НАЧАТЬ СКРОЛИТЬ.
 document.addEventListener('touchmove', function (event) {
-	if (event.scale !== 1) { event.preventDefault(); }
-  }, { passive: false });
+	if (event.scale !== 1) {
+		event.preventDefault();
+	}
+}, {
+	passive: false
+});
+
+document.querySelector(".burger").addEventListener("click", function () {
+	let active = this.classList.toggle("burger--active");
+	if(active) {
+		document.querySelector(".nav__items").style.display = "block"
+	} else {
+		document.querySelector(".nav__items").style.display = "none"
+	}
+});
